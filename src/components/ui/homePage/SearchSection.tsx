ade1c50eeafcbb88be43dcc10ed5dcc6f5170189
s1hwa <142314061+s1hwa@users.noreply.github.com>
2025-05-25 23:19:44 +0900

    검색창 아이콘 및 여백 수정

--- src/components/ui/homePage/SearchSection.tsx
+++ src/components/ui/homePage/SearchSection.tsx
@@ -16,7 +16,7 @@ const SearchSection: React.FC = () => {
       return;
     }
 
-    const currentLang = i18n.language.toLowerCase(); 
+    const currentLang = i18n.language.toLowerCase(); // 예: "ko"
 
     try {
       const response = await fetch('http://localhost:8080/places/search', {
@@ -35,7 +35,7 @@ const SearchSection: React.FC = () => {
         throw new Error(`서버 오류: ${response.status}`);
       }
 
-      const data: SearchResultModel[] = await response.json(); 
+      const data: SearchResultModel[] = await response.json();
 
       console.log('Response:', response);
       console.log('Data:', data);
@@ -54,7 +54,14 @@ const SearchSection: React.FC = () => {
       <div className="relative w-full max-w-[609px]">
         <form onSubmit={handleSearch} className="w-full">
           <div className="flex items-center h-[80px] w-full bg-white rounded-[40px] shadow-[0px_4px_30px_rgba(0,0,0,0.25)]">
-            <div className="flex items-center pl-6">
+            <input
+              type="text"
+              placeholder={t('searchPrompt')}
+              className="h-full flex-grow px-4 pl-8 text-[24px] text-[#919191] font-['LG_PC'] focus:outline-none rounded-[40px]"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="flex items-center ml-auto pr-6">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -69,14 +76,7 @@ const SearchSection: React.FC = () => {
                 <circle cx="11" cy="11" r="8" />
                 <line x1="21" y1="21" x2="16.65" y2="16.65" />
               </svg>
-            </div>
-            <input
-              type="text"
-              placeholder={t('searchPrompt')}
-              className="h-full flex-grow px-4 text-[24px] text-[#919191] font-['LG_PC'] focus:outline-none rounded-[40px]"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
+            </button>
           </div>
         </form>
       </div>
